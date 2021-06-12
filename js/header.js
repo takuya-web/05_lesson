6e5358a7043196b04618f855ed40d93ba8a3dde3
Takuya Okamoto <t.okamoto@o-bisu.com>
2021-06-12 22:24:58 +0900

    20210612

--- js/header.js
+++ js/header.js
@@ -1,10 +1,24 @@
 // ヘッダーメニュー／マウスオーバーアニメーション
 $(function(){
   $(".p-header__nav_item").mouseover(function(){
-    $(this).children(".p-header__nav_list-sub").stop().slideDown(150);
+    $(this).children(".p-header__nav_list-sub").stop().slideDown();
   });
   $(".p-header__nav_item").mouseout(function(){
     $(".p-header__nav_list-sub").stop().slideUp(0);
+  });
+});
+
+$(function(){
+  $(".c-hamburger").click(function(){
+    $(".p-header__nav__rp").slideToggle(200);
+  });
+});
+// メニューの中身
+$(function() {
+  $('.p-header__ac-parent').hover(function() {
+    $(this).children('.p-header__ac-child').slideToggle(200);
+    // openクラスをつける
+    $(this).toggleClass("open");
   });
 });
 
